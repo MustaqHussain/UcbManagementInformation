f2ab27dfe1931fbe6734b4a6fc97c78feff16dc8
MustaqHussain <mustaq.hussain@capgemini.com>
2018-08-18 00:35:20 +0100

    Rename .ignore->txt

---
+++ Packages/Properties/AssemblyInfo.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Packages")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ESF")]
+[assembly: AssemblyProduct("Packages")]
+[assembly: AssemblyCopyright("Copyright © ESF 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d91adc4f-cc53-4432-88cf-48dbf3de0f1f")]
+
+// Assembly Information moved to SolutionAssemblyInfo.cs file under Solution Items
+// Link file added to Project
+
